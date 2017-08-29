32928acdb19aa1b273bbc8ade86bb2a6df0310f1
Sarah Mestiri <mestiri.sa@gmail.com>
2017-08-29 09:39:12 +0100

    Email Spam Filter Project

---
+++ Email-Spam-Filter/Email-spam-filter.py
@@ -0,0 +1,68 @@
+import os
+from collections import Counter
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB, GaussianNB, BernoulliNB
+from sklearn.svm import SVC, NuSVC, LinearSVC
+from sklearn.metrics import confusion_matrix, classification_report
+import numpy as np
+def make_Dictionary(train_dir):
+    emails = [os.path.join(train_dir, f) for f in os.listdir(train_dir)]
+    all_words = []
+    for mail in emails:
+        with open(mail) as m:
+            for i, line in enumerate(m):
+                if i == 2:  # Body of email is only 3rd line of text file
+                    words = line.split()
+                    all_words += words
+
+    dictionary = Counter(all_words)
+    #  non-word removal here
+    list_to_remove = dictionary.keys()
+    for item in list(list_to_remove):
+        if item.isalpha() == False:
+            del dictionary[item]
+        elif len(item) == 1:
+            del dictionary[item]
+    dictionary = dictionary.most_common(5000)
+    return dictionary
+
+def extract_features(mail_dir):
+    files = [os.path.join(mail_dir,fi) for fi in os.listdir(mail_dir)]
+    features_matrix = np.zeros((len(files),5000))
+    docID = 0;
+    for fil in files:
+      with open(fil) as fi:
+        for i,line in enumerate(fi):
+          if i == 2:
+            words = line.split()
+            for word in words:
+              wordID = 0
+              for i,d in enumerate(dictionary):
+                if d[0] == word:
+                  wordID = i
+                  features_matrix[docID,wordID] = words.count(word)
+        docID = docID + 1
+    return features_matrix
+
+train_dir = 'ling-spam\\train-mails'
+dictionary = make_Dictionary(train_dir)
+print (dictionary)
+train_labels = np.zeros(702)
+train_labels[351:701] = 1 #spam emails
+train_matrix = extract_features(train_dir)
+
+# Training SVM and Naive bayes classifier
+model1 = MultinomialNB()
+model2 = LinearSVC()
+model1.fit(train_matrix,train_labels)
+model2.fit(train_matrix,train_labels)
+
+# Test the unseen mails for Spam
+test_dir = 'ling-spam\\test-mails'
+test_matrix = extract_features(test_dir)
+test_labels = np.zeros(260)
+test_labels[130:260] = 1 #spam emails
+result1 = model1.predict(test_matrix)
+result2 = model2.predict(test_matrix)
+print(confusion_matrix(test_labels,result1))
+print(confusion_matrix(test_labels,result2))
