779d89920d17ea29d539b6960e71c0d7321121f5
Sarah Mestiri <mestiri.sa@gmail.com>
2017-09-01 18:58:33 +0100

    Trying SVM and tuning parameters

--- Email-Spam-Filter/Email-spam-filter.py
+++ Email-Spam-Filter/Email-spam-filter.py
@@ -24,12 +24,12 @@ def make_Dictionary(train_dir):
             del dictionary[item]
         elif len(item) == 1:
             del dictionary[item]
-    dictionary = dictionary.most_common(500)
+    dictionary = dictionary.most_common(5000)
     return dictionary
 
 def extract_features(mail_dir):
     files = [os.path.join(mail_dir,fi) for fi in os.listdir(mail_dir)]
-    features_matrix = np.zeros((len(files),500))
+    features_matrix = np.zeros((len(files),5000))
     docID = 0;
     for fil in files:
       with open(fil) as fi:
@@ -51,27 +51,43 @@ print (dictionary)
 train_labels = np.zeros(702)
 train_labels[351:701] = 1 #spam emails
 train_matrix = extract_features(train_dir)
-
+print(train_matrix[1])
 # Training SVM and Naive bayes classifier
 NB_model = MultinomialNB()
 #GNB_model = GaussianNB()
-BNB_model = BernoulliNB()
-#SVM_model = LinearSVC()
+#BNB_model = BernoulliNB()
+LinearSVM_model = LinearSVC()
+SVM_model = SVC()
 NB_model.fit(train_matrix,train_labels)
 #GNB_model.fit(train_matrix,train_labels)
-BNB_model.fit(train_matrix,train_labels)
-#SVM_model.fit(train_matrix,train_labels)
+#BNB_model.fit(train_matrix,train_labels)
+LinearSVM_model.fit(train_matrix,train_labels)
+SVM_model.fit(train_matrix,train_labels)
+
+#use GridSearch to better choose tuning parameters C and gamma
+param_grid = {'C':[0.1,1,10,100,1000],'gamma':[1,0.1,0.01,0.001,0.0001]}
+grid = GridSearchCV(SVC(), param_grid, verbose=3)
+grid.fit(train_matrix,train_labels)
+
+print(grid.best_params_)
+print(grid.best_estimator_)
 
 # Test the unseen mails for Spam
 test_dir = 'ling-spam\\test-mails'
 test_matrix = extract_features(test_dir)
 test_labels = np.zeros(260)
 test_labels[130:260] = 1 #spam emails
+
 NB_predictions = NB_model.predict(test_matrix)
 #GNB_predictions = GNB_model.predict(test_matrix)
-BNB_predictions = BNB_model.predict(test_matrix)
-#SVM_predictions = SVM_model.predict(test_matrix)
+#BNB_predictions = BNB_model.predict(test_matrix)
+LinearSVM_predictions = LinearSVM_model.predict(test_matrix)
+grid_predictions = grid.predict(test_matrix)
+SVM_predictions = SVM_model.predict(test_matrix)
+
 print(confusion_matrix(test_labels,NB_predictions))
-#print(confusion_matrix(test_labels,SVM_predictions))
+print(confusion_matrix(test_labels,LinearSVM_predictions))
+print(confusion_matrix(test_labels,SVM_predictions))
+print(confusion_matrix(test_labels,grid_predictions))
 #print(confusion_matrix(test_labels,GNB_predictions))
-print(confusion_matrix(test_labels,BNB_predictions))
+#print(confusion_matrix(test_labels,BNB_predictions))
